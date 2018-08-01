aca484ce41265d9edc264be75222076acad07432
DanielCrom1 <DanielCrom1@gmail.com>
2018-08-01 12:09:35 -0700

    Initial Commit

---
+++ CurrencyConverter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CurrencyConverter
+//
+//  Created by Колик <3 on 8/1/18.
+//  Copyright © 2018 MPrak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CurrencyConverter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CurrencyConverter
+//
+//  Created by Колик <3 on 8/1/18.
+//  Copyright © 2018 MPrak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
